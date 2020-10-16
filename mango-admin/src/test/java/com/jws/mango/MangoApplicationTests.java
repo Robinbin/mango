66af7b09bc8aa08b3c4b3d4c49d47ed0760c191f
Jack <hansenzhulei@qq.com>
2020-10-16 22:24:35 +0800

    Add Jenkins pipeline

--- mango-admin/src/test/java/com/jws/mango/MangoApplicationTests.java
+++ mango-admin/src/test/java/com/jws/mango/MangoApplicationTests.java
@@ -3,11 +3,66 @@ package com.jws.mango;
 import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class MangoApplicationTests {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-//	@Test
-	void contextLoads() {
-	}
+//@SpringBootTest
+public class MangoApplicationTests {
 
+    //	@Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void testJava8() {
+        List<Apple> list = new ArrayList<>();
+        Apple a1 = new Apple("Red", 12);
+        Apple a2 = new Apple("Blue", 212);
+        Apple a3 = new Apple("Yellow", 132);
+        Apple a4 = new Apple("Black", 16);
+        Apple a5 = new Apple("Green", 6);
+        Apple a6 = new Apple("Pink", 26);
+
+        list.add(a1);
+        list.add(a2);
+        list.add(a3);
+        list.add(a4);
+        list.add(a5);
+        list.add(a6);
+        System.out.println(getList(list, (apple) -> apple.getWeight() > 15));
+        System.out.println(getList(list, (apple) -> apple.getName().equals("Yellow")));
+    }
+
+    private List<String> getList(List<Apple> apples, Predicate<Apple> p) {
+        return apples.stream().filter(p).map(Apple::getName).sorted(Comparator.naturalOrder()).collect(Collectors.toList());
+    }
+
+    private static class Apple {
+        String name;
+        int weight;
+
+        public Apple(String name, int weight) {
+            this.name = name;
+            this.weight = weight;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getWeight() {
+            return weight;
+        }
+
+        public void setWeight(int weight) {
+            this.weight = weight;
+        }
+    }
 }
